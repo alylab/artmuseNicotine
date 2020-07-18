aeddc169f1440741f8695de521e25acf1a1945de
monicathieu <mkt2131@columbia.edu>
2020-07-18 11:31:28 -0400

    keep more smoking covariates from demos

--- R/preprocess.R
+++ R/preprocess.R
@@ -46,7 +46,7 @@ raw = tibble(filename = list.files(data_dir_task, pattern = ".txt", full.names =
   # create "on_smoking" based on "session_order"
   # which indicates which session (1 or 2) was the smoking session
   left_join(demos %>%
-              select(subj_num, session_order, ppm_on, ppm_off) %>%
+              select(subj_num, session_order, ppm_on, ppm_off, cigs_per_day_est, ftnd, years_smoke) %>%
               pivot_longer(names_to = "on_smoking", values_to = "ppm", cols = starts_with("ppm")) %>% 
               mutate(on_smoking = recode(on_smoking, ppm_on = 1L, ppm_off = 0L),
                      # move session_num to 0 and 1, I gotta plan
