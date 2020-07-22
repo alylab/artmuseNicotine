0fc11b7bea498af1a417f606c98c81ff674fef3c
monicathieu <mkt2131@columbia.edu>
2020-07-22 11:35:52 -0400

    tidy all coefs in boot

--- R/model_boot.R
+++ R/model_boot.R
@@ -124,8 +124,8 @@ aprime_by_ppm_boot <- boots_by_subj %>%
                      coefs_main = model_main,
                      coefs_covar = model_covar,
                      coefs_diffonly = model_diffonly) %>%
-              mutate(coefs_main = map(coefs_main,
-                                     ~broom::tidy(.))),
+              mutate_at(vars(starts_with("coefs")),
+                        ~map(., ~broom::tidy(.x))),
             by = c("metric_type", "exptCond", "probe"),
             suffix = c("_boot", "_raw")) %>%
   mutate(predicted_resid = map2(data_raw, model_resid,
